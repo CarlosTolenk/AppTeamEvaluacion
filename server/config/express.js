0f21d92bdd5a8f30c71185c954671ca0c3285774
Carlos Tolentino <carlos.tolentinoe@gmail.com>
2018-02-20 16:54:38 -0400

    Enrutamiento entre Angular y Express exitoso

--- server/config/express.js
+++ server/config/express.js
@@ -1,10 +1,11 @@
-var logger = require('morgan');
-var bodyParser = require('body-parser');
-var swig = require('swig');
-var express = require('express')
+// Importando los modulos necesarios para la configuraciones de Express
+const logger = require('morgan');
+const bodyParser = require('body-parser');
+const swig = require('swig');
+const express = require('express')
 
 
-module.exports = function(app, config){
+module.exports = (app, config) => {
 
 	//Asignamos un motor de templates a Node.js -- Swig
 	app.engine('html', swig.renderFile);
@@ -14,13 +15,13 @@ module.exports = function(app, config){
 	//Para no cachear HTML en el entorno de desarrollo
 	app.set('view cache', false);
 	//No cacheamos el HTML
-	swig.setDefaults({ cache: false});
+	swig.setDefaults({ cache: false, varControl: ['{^','^}']});
 
 
 
 	app.use(logger('dev'));
 	app.use(bodyParser());
-	//Cambió
+	//Asignamos la ruta estática de nuestro frontend, el config.rootPath esta el __dirname
 	app.use(express.static(config.rootPath + '/public'));
 
 };
